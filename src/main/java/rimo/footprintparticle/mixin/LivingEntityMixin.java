8b3369069676180102d1c5c7377e571d1bb39b9f
Rimo <113169310+Rivmun@users.noreply.github.com>
2023-03-05 22:41:54 +0800

    0.2.1 (#8)
    - Toggle for invisible entity gen.
    - Block height offset list.
    - Print height offset to prevent gen underground.

--- src/main/java/rimo/footprintparticle/mixin/LivingEntityMixin.java
+++ src/main/java/rimo/footprintparticle/mixin/LivingEntityMixin.java
@@ -2,7 +2,6 @@ package rimo.footprintparticle.mixin;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
@@ -45,46 +44,65 @@ public abstract class LivingEntityMixin extends Entity {
 	}
 
 	public void footprintGenerator() {
-		if (FPPClient.CONFIG.isEnable()) {
-			if (!FPPClient.CONFIG.getExcludedMobs().contains(EntityType.getId(this.getType()).toString())) {
-				timer = this.isSprinting() ? (int) (FPPClient.CONFIG.getSecPerPrint() * 13.33f) : (int) (FPPClient.CONFIG.getSecPerPrint() * 20);
+		if (!FPPClient.CONFIG.isEnable())
+			return;
+		if (FPPClient.CONFIG.getExcludedMobs().contains(EntityType.getId(this.getType()).toString()))
+			return;
+		if (!FPPClient.CONFIG.getCanGenWhenInvisible() && this.isInvisible())
+			return;
 
-				// Fix pos...
-				var px = this.getX();
-				var py = this.getY() + 0.01f;
-				var pz = this.getZ();
+		timer = this.isSprinting() ? (int) (FPPClient.CONFIG.getSecPerPrint() * 13.33f) : (int) (FPPClient.CONFIG.getSecPerPrint() * 20);
 
-				// Horse and spider pos set on besides...
-				if (FPPClient.CONFIG.getHorseLikeMobs().contains(EntityType.getId(this.getType()).toString())) {
-					var i = Math.random() > 0.5f ? 1 : -1;		// Random sides
-					px = px + 0.75f * i * Math.sin(this.getHorizontalFacing().asRotation() / 180 * Math.PI);
-					pz = pz + 0.75f * i * Math.cos(this.getHorizontalFacing().asRotation() / 180 * Math.PI);
-					timer = (int) (this.getPrimaryPassenger() != null ? this.getPrimaryPassenger().isPlayer() ? timer * 0.5f : timer * 1.33f : timer * 1.33f);
-				}
-				if (FPPClient.CONFIG.getSpiderLikeMobs().contains(EntityType.getId(this.getType()).toString())) {
-					var i = Math.random() > 0.5f ? 1 : -1;
-					px = px + 0.9f * i * Math.cos(this.getHorizontalFacing().asRotation() / 180 * Math.PI);
-					pz = pz + 0.9f * i * Math.sin(this.getHorizontalFacing().asRotation() / 180 * Math.PI);
-					timer *= 0.66f;
-				}
+		// Fix pos...
+		var px = this.getX();
+		var py = this.getY() + 0.01f + FPPClient.CONFIG.getPrintHeight();
+		var pz = this.getZ();
+
+		// Horse and spider pos set on besides...
+		if (FPPClient.CONFIG.getHorseLikeMobs().contains(EntityType.getId(this.getType()).toString())) {
+			var i = Math.random() > 0.5f ? 1 : -1;		// Random sides
+			px = px + 0.75f * i * Math.sin(this.getHorizontalFacing().asRotation() / 180 * Math.PI);
+			pz = pz + 0.75f * i * Math.cos(this.getHorizontalFacing().asRotation() / 180 * Math.PI);
+			timer = (int) (this.getPrimaryPassenger() != null ? this.getPrimaryPassenger().isPlayer() ? timer * 0.5f : timer * 1.33f : timer * 1.33f);
+		}
+		if (FPPClient.CONFIG.getSpiderLikeMobs().contains(EntityType.getId(this.getType()).toString())) {
+			var i = Math.random() > 0.5f ? 1 : -1;
+			px = px + 0.9f * i * Math.cos(this.getHorizontalFacing().asRotation() / 180 * Math.PI);
+			pz = pz + 0.9f * i * Math.sin(this.getHorizontalFacing().asRotation() / 180 * Math.PI);
+			timer *= 0.66f;
+		}
 	
-				// Check block type...
-				var block = this.world.getBlockState(new BlockPos(px, py, pz));
-				var canGen = isPrintCanGen(block);
-				if (!canGen) {
-					block = this.world.getBlockState(new BlockPos(px, py - 1, pz));
-					canGen = isPrintCanGen(block);
-				} else {
-					// Fix pos for some special blocks if is in.
-					if (block.isOf(Blocks.SNOW) || block.isOf(Blocks.SOUL_SAND))
-						py += 0.125f;
+		// Check block type...
+		var block = this.world.getBlockState(new BlockPos(px, py, pz));
+		var canGen = isPrintCanGen(block);
+		if (!canGen) {
+			block = this.world.getBlockState(new BlockPos(px, py - 1, pz));
+			canGen = isPrintCanGen(block);
+		} else {
+			// Fix height by blocks if in...
+			try {
+				for (String str : FPPClient.CONFIG.getBlockHeight()) {
+					String[] str2 = str.split(",");
+					if (str2[0].charAt(0) == '#') {
+						for (TagKey<Block> tag : block.streamTags().toList()) {
+							if (str2[0].equals("#" + tag.id().toString())) {
+								py += Float.parseFloat(str2[1]);
+								break;
+							}
+						}
+					} else if (str2[0].contentEquals(block.getRegistryEntry().getKey().get().getValue().toString())) {
+						py += Float.parseFloat(str2[1]);
+						break;
+					}
 				}
-	
-				if (canGen) {
-					FootprintParticleType footprint = FPPClient.FOOTPRINT;
-					this.world.addParticle(footprint.setData((LivingEntity) (Object) this), px, py, pz, this.getVelocity().getX(), 0, this.getVelocity().getZ());
-				}
+			} catch (Exception e) {
+				// Ignore...
 			}
+		}
+	
+		if (canGen) {
+			FootprintParticleType footprint = FPPClient.FOOTPRINT;
+			this.world.addParticle(footprint.setData((LivingEntity) (Object) this), px, py, pz, this.getVelocity().getX(), 0, this.getVelocity().getZ());
 		}
 	}
 
