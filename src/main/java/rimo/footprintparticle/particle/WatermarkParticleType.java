1e89e2a1433ebcbd4a737f66675db212a859602f
Rimo <113169310+Rivmun@users.noreply.github.com>
2023-06-27 19:28:03 +0800

    Add files via upload

---
+++ src/main/java/rimo/footprintparticle/particle/WatermarkParticleType.java
@@ -0,0 +1,9 @@
+package rimo.footprintparticle.particle;
+
+public class WatermarkParticleType extends FootprintParticleType {
+
+	public WatermarkParticleType(boolean alwaysShow) {
+		super(alwaysShow);
+	}
+
+}
