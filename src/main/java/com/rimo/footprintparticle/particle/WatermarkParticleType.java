c14fc2a1845bb7834046e82a3b3f6e68242007db
Rimo <113169310+Rivmun@users.noreply.github.com>
2023-07-17 02:52:04 +0800

    Delete src/main directory

--- src/main/java/com/rimo/footprintparticle/particle/WatermarkParticleType.java
+++
@@ -1,9 +0,0 @@
-package com.rimo.footprintparticle.particle;
-
-public class WatermarkParticleType extends FootprintParticleType {
-
-	public WatermarkParticleType(boolean alwaysShow) {
-		super(alwaysShow);
-	}
-
-}
