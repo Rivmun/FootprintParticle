012ec1edbfcfd7b9612885e8d952004fef7cba5e
Rimo <113169310+Rivmun@users.noreply.github.com>
2023-07-17 04:49:37 +0800

    Architectify (#36)
    * Delete src/main directory
    
    * Add files via upload
    
    * Update gradle.yml

---
+++ forge/src/main/java/com/rimo/footprintparticle/FPPForge.java
@@ -0,0 +1,32 @@
+package com.rimo.footprintparticle;
+
+import com.rimo.footprintparticle.config.ConfigScreen;
+import dev.architectury.platform.forge.EventBuses;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.ConfigGuiHandler;
+import net.minecraftforge.fml.DistExecutor;
+import net.minecraftforge.fml.IExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.network.NetworkConstants;
+
+@Mod(FPPClient.MOD_ID)
+public class FPPForge {
+	public FPPForge() {
+		// Submit our event bus to let architectury register our content on the right time
+		EventBuses.registerModEventBus(FPPClient.MOD_ID, FMLJavaModLoadingContext.get().getModEventBus());
+		FPPClient.onInitializeClient();
+
+		ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> NetworkConstants.IGNORESERVERONLY, (a, b) -> true));
+		DistExecutor.safeRunWhenOn(Dist.CLIENT, () -> FPPForge::registerModsPage);
+
+		DistExecutor.safeRunWhenOn(Dist.DEDICATED_SERVER, () -> () -> FPPClient.LOGGER.warn("'FootprintParticle' is a client side mod, it's should be removed from server mod folder."));
+	}
+
+	public static void registerModsPage() {
+		ModLoadingContext.get().registerExtensionPoint(ConfigGuiHandler.ConfigGuiFactory.class, () -> new ConfigGuiHandler.ConfigGuiFactory((client, parent) -> {
+			return new ConfigScreen().buildScreen();
+		}));
+	}
+}
