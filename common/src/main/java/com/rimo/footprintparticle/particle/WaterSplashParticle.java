b587b19ffedcde342175ff7ef28f0769851728ec
Rimo <113169310+Rivmun@users.noreply.github.com>
2023-09-21 18:18:34 +0800

    0.3.3 (#43)

---
+++ common/src/main/java/com/rimo/footprintparticle/particle/WaterSplashParticle.java
@@ -0,0 +1,39 @@
+package com.rimo.footprintparticle.particle;
+
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.particle.Particle;
+import net.minecraft.client.particle.ParticleFactory;
+import net.minecraft.client.particle.RainSplashParticle;
+import net.minecraft.client.particle.SpriteProvider;
+import net.minecraft.client.world.ClientWorld;
+import net.minecraft.particle.DefaultParticleType;
+import net.minecraft.util.math.BlockPos;
+
+public class WaterSplashParticle extends RainSplashParticle {
+
+	// Vanilla's splash & rain particle can't apply vy (h), so we made custom one to override it.
+	protected WaterSplashParticle(ClientWorld clientWorld, double d, double e, double f, double g, double h, double i) {
+		super(clientWorld, d, e, f);
+		this.gravityStrength = 0.04F;
+		this.velocityX = g;
+		this.velocityY = 0.1f + h;
+		this.velocityZ = i;
+		this.maxAge *= 10;
+	}
+
+	@Environment(EnvType.CLIENT)
+	public static class DefaultFactory implements ParticleFactory<DefaultParticleType> {
+		private final SpriteProvider spriteProvider;
+
+		public DefaultFactory(SpriteProvider spriteProvider) {
+			this.spriteProvider = spriteProvider;
+		}
+
+		public Particle createParticle(DefaultParticleType defaultParticleType, ClientWorld clientWorld, double d, double e, double f, double g, double h, double i) {
+			WaterSplashParticle waterSplashParticle = new WaterSplashParticle(clientWorld, d, e, f, g, h, i);
+			waterSplashParticle.setSprite(this.spriteProvider);
+			return waterSplashParticle;
+		}
+	}
+}
