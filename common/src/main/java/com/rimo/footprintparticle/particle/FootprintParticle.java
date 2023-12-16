52e1a63e99263f6197495378dbbce059cb8f5ebb
Rimo <113169310+Rivmun@users.noreply.github.com>
2023-12-17 01:21:16 +0800

    0.5.0 #61 (#60)
    - Add switcher for enable mod option.
    - Add support for custom texture.
    - Add compat for `Random Mob Sizes`.
    - Other optimizations.

--- common/src/main/java/com/rimo/footprintparticle/particle/FootprintParticle.java
+++ common/src/main/java/com/rimo/footprintparticle/particle/FootprintParticle.java
@@ -1,33 +1,41 @@
 package com.rimo.footprintparticle.particle;
 
 import com.rimo.footprintparticle.FPPClient;
+import com.rimo.footprintparticle.Util;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.particle.*;
 import net.minecraft.client.render.Camera;
 import net.minecraft.client.render.VertexConsumer;
+import net.minecraft.client.texture.Sprite;
 import net.minecraft.client.world.ClientWorld;
+import net.minecraft.entity.EntityType;
 import net.minecraft.particle.DefaultParticleType;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.MathHelper;
-import net.minecraft.util.math.Quaternion;
-import net.minecraft.util.math.Vec3f;
+import net.minecraft.util.math.*;
+
+import java.util.List;
 
 public class FootprintParticle extends SpriteBillboardParticle {
-	public final SpriteProvider spriteProvider;
 	protected float startAlpha;
 
-	protected FootprintParticle(ClientWorld clientWorld, double x, double y, double z, double vx, double vy, double vz, SpriteProvider spriteProvider) {
+	protected FootprintParticle(ClientWorld clientWorld, double x, double y, double z, double vx, double vy, double vz, SpriteProvider spriteProvider, FootprintParticleType parameters, String defName) {
 		super(clientWorld, x, y, z, vx, vy, vz);
-
-		this.spriteProvider = spriteProvider;
-		this.setSprite(spriteProvider.getSprite(random));
 
 		this.setVelocity(0, 0, 0);
 		this.setAlpha(FPPClient.CONFIG.getFootprintAlpha());
 		this.angle = (float) MathHelper.atan2(vx, vz);
 		this.maxAge = (int) (FPPClient.CONFIG.getPrintLifetime() * 20);
-		this.scale = FPPClient.CONFIG.getFootprintSize();
+		this.scale = FPPClient.CONFIG.getFootprintSize() * 0.03125f;
+
+		this.scale *= Util.getEntityScale((parameters.entity));
+
+		List<Sprite> spriteList = Util.getCustomSprites(parameters.entity, spriteProvider, defName);
+		try {
+			this.setSprite(spriteList.get((int) (Math.random() * spriteList.size())));
+		} catch (Exception e) {
+			FPPClient.LOGGER.error("Wrong custom texture for " + EntityType.getId(parameters.entity.getType()).toString() + ", please check.");
+			this.setSprite(spriteProvider);
+		}
 	}
 
 	@Override
@@ -49,42 +57,50 @@ public class FootprintParticle extends SpriteBillboardParticle {
 		if (this.age > this.maxAge / 2)
 			this.alpha -= this.startAlpha / this.maxAge * 2;
 
-		if (this.age++ >= this.maxAge || this.world.isAir(new BlockPos(this.x, this.y - 0.02f, this.z)))
+		if (this.age++ >= this.maxAge || this.world.isAir(new BlockPos(MathHelper.floor(this.x), MathHelper.floor(this.y - 0.02f), MathHelper.floor(this.z))))
 			this.markDead();
 	}
 
 	@Override
 	public void buildGeometry(VertexConsumer vertexConsumer, Camera camera, float tickDelta) {
-		var camPos = camera.getPos();
-		var x = (float) (MathHelper.lerp(tickDelta, this.prevPosX, this.x) - camPos.getX());
-		var y = (float) (MathHelper.lerp(tickDelta, this.prevPosY, this.y) - camPos.getY());
-		var z = (float) (MathHelper.lerp(tickDelta, this.prevPosZ, this.z) - camPos.getZ());
-		Vec3f[] pos = new Vec3f[]{new Vec3f(-1, 0, -1), new Vec3f(-1, 0, 1), new Vec3f(1, 0, 1), new Vec3f(1, 0, -1)};		//TODO: why cannot use vec3d here?
+		Vec3d camPos = camera.getPos();
+		float x = (float)(MathHelper.lerp(tickDelta, this.prevPosX, this.x) - camPos.getX());
+		float y = (float)(MathHelper.lerp(tickDelta, this.prevPosY, this.y) - camPos.getY());
+		float z = (float)(MathHelper.lerp(tickDelta, this.prevPosZ, this.z) - camPos.getZ());
+		/*
+		 * In Minecraft,
+		 * rotate a vec3f point P around an axis with θ radian (anticlockwise)
+		 * needs a NORMALIZED normal vec3f(x, y, z) direct to this axis direction
+		 * then simply call P.rotate(new Quaternion(sinθ*x, sinθ*y, sinθ*z, cosθ)) with half the θ
+		 * Oh, MAGIC! (👈 he is totally idiot.)
+		 *
+		 * here we need vertex rotate around Y axis, so the normal is (0, 1, 0), that's let X and Z are 0.
+		 */
+		Quaternion q = new Quaternion(
+				0,
+				MathHelper.sin(this.angle / 2),
+				0,
+				MathHelper.cos(this.angle / 2)
+		);
+		Vec3f[] pos = new Vec3f[]{new Vec3f(-1, 0, -1), new Vec3f(-1, 0, 1), new Vec3f(1, 0, 1), new Vec3f(1, 0, -1)};
+		float i = this.getSize(tickDelta);
 
-		for (int k = 0; k < 4; ++k) {
-			/*
-			 * In Minecraft,
-			 * rotate a vec3f point P around a axis with θ radian (anticlockwise)
-			 * needs a vec3f(x, y, z).normalize() direct to this axis direction
-			 * then simply call P.rotate(new Quaternion(sinθ*x, sinθ*y, sinθ*z, cosθ)) with half the θ
-			 * Oh, MAGIC! (👈 he is totally idiot.)
-			 *
-			 * here we need vertex rotate around Y axis, so leave X and Z for zero.
-			 */
-			pos[k].rotate(new Quaternion(
-					0,
-					MathHelper.sin(this.angle / 2),
-					0,
-					MathHelper.cos(this.angle / 2)
-			));
-			pos[k].scale(this.getSize(tickDelta));
-			pos[k].add(x, y, z);
+		for (int j = 0; j < 4; ++j) {
+			Vec3f vec3f = pos[j];
+			vec3f.rotate(q);
+			vec3f.scale(i);
+			vec3f.add(x, y, z);
 		}
 
-		vertexConsumer.vertex(pos[0].getX(), pos[0].getY(), pos[0].getZ()).texture(this.getMaxU(), this.getMaxV()).color(this.red, this.green, this.blue, this.alpha).light(this.getBrightness(tickDelta)).next();
-		vertexConsumer.vertex(pos[1].getX(), pos[1].getY(), pos[1].getZ()).texture(this.getMaxU(), this.getMinV()).color(this.red, this.green, this.blue, this.alpha).light(this.getBrightness(tickDelta)).next();
-		vertexConsumer.vertex(pos[2].getX(), pos[2].getY(), pos[2].getZ()).texture(this.getMinU(), this.getMinV()).color(this.red, this.green, this.blue, this.alpha).light(this.getBrightness(tickDelta)).next();
-		vertexConsumer.vertex(pos[3].getX(), pos[3].getY(), pos[3].getZ()).texture(this.getMinU(), this.getMaxV()).color(this.red, this.green, this.blue, this.alpha).light(this.getBrightness(tickDelta)).next();
+		float k = this.getMinU();
+		float l = this.getMaxU();
+		float n = this.getMaxV();
+		float m = this.getMinV();
+		int o = this.getBrightness(tickDelta);
+		vertexConsumer.vertex(pos[0].getX(), pos[0].getY(), pos[0].getZ()).texture(l, n).color(this.red, this.green, this.blue, this.alpha).light(o).next();
+		vertexConsumer.vertex(pos[1].getX(), pos[1].getY(), pos[1].getZ()).texture(l, m).color(this.red, this.green, this.blue, this.alpha).light(o).next();
+		vertexConsumer.vertex(pos[2].getX(), pos[2].getY(), pos[2].getZ()).texture(k, m).color(this.red, this.green, this.blue, this.alpha).light(o).next();
+		vertexConsumer.vertex(pos[3].getX(), pos[3].getY(), pos[3].getZ()).texture(k, n).color(this.red, this.green, this.blue, this.alpha).light(o).next();
 	}
 
 	@Environment(EnvType.CLIENT)
@@ -97,10 +113,7 @@ public class FootprintParticle extends SpriteBillboardParticle {
 
 		@Override
 		public Particle createParticle(DefaultParticleType parameters, ClientWorld world, double x, double y, double z, double velocityX, double velocityY, double velocityZ) {
-			FootprintParticle particle = new FootprintParticle(world, x, y, z, velocityX, velocityY, velocityZ, this.spriteProvider);
-			if (parameters instanceof FootprintParticleType footprintParameters)
-				particle.scale *= FPPClient.getEntityScale((footprintParameters.entity));
-			return particle;
+			return new FootprintParticle(world, x, y, z, velocityX, velocityY, velocityZ, this.spriteProvider, (FootprintParticleType) parameters, "footprint");
 		}
 	}
 
