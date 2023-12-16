c51caa0b9ebde8bddfdea1713b7744d5f6417eac
Rimo <113169310+Rivmun@users.noreply.github.com>
2023-12-16 22:18:24 +0800

    0.5.0 #58

--- common/src/main/java/com/rimo/footprintparticle/config/FPPConfig.java
+++ common/src/main/java/com/rimo/footprintparticle/config/FPPConfig.java
@@ -74,15 +74,18 @@ public class FPPConfig implements ConfigData {
 			"minecraft:iron_golem,2",
 			"minecraft:creeper,0.8"
 	);
+	static final List<String> DEF_CUSTOM_PRINT = Arrays.asList(
+			"mod_id:mob_id,fileName_NoExtend"
+	);
 
-	private boolean enableMod = true;
+	private int enableMod = 2;
 	private float secPerPrint = 0.5f;
 	private float printLifetime = 5.0f;
 	private float printHeight = 0f;
 	private int wetDuration = 10;
 	private float watermarkAlpha = 0.4f;
 	private float footprintAlpha = 0.7f;
-	private float footprintSize = 0.15625f;
+	private int footprintSize = 5;
 	private boolean canGenWhenInvisible = true;
 	private List<String> applyBlocks = DEF_APPLYBLOCKS;
 	private List<String> blockHeight = DEF_BLOCKHEIGHT;
@@ -97,8 +100,9 @@ public class FPPConfig implements ConfigData {
 	private int swimPopLevel = 2;
 	private int snowDustLevel = 2;
 	private int waterSplashLevel = 1;
+	private List<String> customPrint = DEF_CUSTOM_PRINT;
 
-	public boolean isEnable() {return enableMod;}
+	public int isEnable() {return enableMod;}
 	public float getSecPerPrint() {return secPerPrint;}
 	public float getPrintLifetime() {return printLifetime;}
 	public float getPrintHeight() {return printHeight;}
@@ -119,9 +123,10 @@ public class FPPConfig implements ConfigData {
 	public int getSnowDustLevel() {return snowDustLevel;}
 	public int getWaterSplashLevel() {return waterSplashLevel;}
 	public List<String> getMobInterval() {return mobInterval;}
-	public float getFootprintSize() {return footprintSize;}
+	public int getFootprintSize() {return footprintSize;}
+	public List<String> getCustomPrint() {return customPrint;}
 
-	public void setEnableMod(boolean isEnable) {enableMod = isEnable;}
+	public void setEnableMod(int isEnable) {enableMod = isEnable;}
 	public void setSecPerPrint(float sec) {secPerPrint = sec;}
 	public void setPrintLifetime(float time) {printLifetime = time;}
 	public void setPrintHeight(float height) {printHeight = height;}
@@ -142,5 +147,6 @@ public class FPPConfig implements ConfigData {
 	public void setSnowDustLevel(int snowDustLevel) {this.snowDustLevel = snowDustLevel;}
 	public void setWaterSplashLevel(int waterSplashLevel) {this.waterSplashLevel = waterSplashLevel;}
 	public void setMobInterval(List<String> mobInterval) {this.mobInterval = mobInterval;}
-	public void setFootprintSize(float footprintSize) {this.footprintSize = footprintSize;}
+	public void setFootprintSize(int footprintSize) {this.footprintSize = footprintSize;}
+	public void setCustomPrint(List<String> customPrint) {this.customPrint = customPrint;}
 }
