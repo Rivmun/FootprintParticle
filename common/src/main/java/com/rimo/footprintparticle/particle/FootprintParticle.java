be6c51808e4ed69c738f46723e5fd3391a77aeab
Rimo <113169310+Rivmun@users.noreply.github.com>
2023-12-16 22:22:48 +0800

    Update Debug aga

--- common/src/main/java/com/rimo/footprintparticle/particle/FootprintParticle.java
+++ common/src/main/java/com/rimo/footprintparticle/particle/FootprintParticle.java
@@ -9,6 +9,7 @@ import net.minecraft.client.render.Camera;
 import net.minecraft.client.render.VertexConsumer;
 import net.minecraft.client.texture.Sprite;
 import net.minecraft.client.world.ClientWorld;
+import net.minecraft.entity.EntityType;
 import net.minecraft.particle.DefaultParticleType;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
