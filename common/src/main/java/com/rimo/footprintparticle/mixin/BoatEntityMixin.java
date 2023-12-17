53d7e18dc02efe4194cf00265697e0eba20e51ce
Rimo <113169310+Rivmun@users.noreply.github.com>
2023-12-17 19:59:07 +0800

    Fix #64

--- common/src/main/java/com/rimo/footprintparticle/mixin/BoatEntityMixin.java
+++ common/src/main/java/com/rimo/footprintparticle/mixin/BoatEntityMixin.java
@@ -25,60 +25,62 @@ public abstract class BoatEntityMixin extends Entity {
 
 	@Inject(method = "tick", at = @At("TAIL"))
 	public void tick(CallbackInfo ci) {
-		int k = (int)(this.getVelocity().length() * 10);
-		while (Math.random() < k-- / 5f) {
-			int i = Math.random() > 0.5f ? 1 : -1;		//TODO: boat's yaw is 90 degrees more than other entity, strange.
-			if (this.checkBoatInWater()) {
-				this.world.addParticle(        // at head
-						ParticleTypes.RAIN,
-						this.getX() + 1.2f * MathHelper.cos((float) Math.toRadians(this.getRotationClient().y + 90 + Math.random() * 30 * i)),
-						(int) this.getY() + 1f,
-						this.getZ() + 1.2f * MathHelper.sin((float) Math.toRadians(this.getRotationClient().y + 90 + Math.random() * 30 * i)),
-						0,
-						0,
-						0
-				);
-				for (int j = 0; j < 2; j++) {
-					this.world.addParticle(        // at paddle
-							FPPClient.WATERSPLASH.get(),
-							this.getX() + i * MathHelper.cos((float) Math.toRadians(this.getRotationClient().y - 10 + Math.random() * 20)),
+		if (FPPClient.CONFIG.isEnableBoatTrail()) {
+			int k = (int)(this.getVelocity().length() * 10);
+			while (Math.random() < k-- / 5f) {
+				int i = Math.random() > 0.5f ? 1 : -1;		//TODO: boat's yaw is 90 degrees more than other entity, strange.
+				if (this.checkBoatInWater()) {
+					this.world.addParticle(        // at head
+							ParticleTypes.RAIN,
+							this.getX() + 1.2f * MathHelper.cos((float) Math.toRadians(this.getRotationClient().y + 90 + Math.random() * 30 * i)),
 							(int) this.getY() + 1f,
-							this.getZ() + i * MathHelper.sin((float) Math.toRadians(this.getRotationClient().y - 10 + Math.random() * 20)),
-							(Math.random() - 0.5f) / 4f,
-							Math.random() * this.getVelocity().length() / 2f,
-							(Math.random() - 0.5f) / 4f
+							this.getZ() + 1.2f * MathHelper.sin((float) Math.toRadians(this.getRotationClient().y + 90 + Math.random() * 30 * i)),
+							0,
+							0,
+							0
 					);
-				}
-				this.world.addParticle(        // at tail
-						ParticleTypes.BUBBLE,
-						this.getX() - 1.2f * MathHelper.cos((float) Math.toRadians(this.getRotationClient().y + 90 + Math.random() * 30 * i)),
-						(int) this.getY() + 0.5f + Math.random() / 2f,
-						this.getZ() - 1.2f * MathHelper.sin((float) Math.toRadians(this.getRotationClient().y + 90 + Math.random() * 30 * i)),
-						this.getVelocity().getX() / 5f,
-						Math.random() / 3f,
-						this.getVelocity().getZ() / 5f
-				);
-			} else {
-				if (Math.random() > 0.5f) {
-					this.world.addParticle(		// at paddle
-							ParticleTypes.CLOUD,
-							this.getX() + i * MathHelper.cos((float) Math.toRadians(this.getRotationClient().y - 10 + Math.random() * 20)),
-							this.getY(),
-							this.getZ() + i * MathHelper.sin((float) Math.toRadians(this.getRotationClient().y - 10 + Math.random() * 20)),
-							Math.random() / 5f,
-							Math.random() / 5f,
-							Math.random() / 5f
-					);
-				} else {
+					for (int j = 0; j < 2; j++) {
+						this.world.addParticle(        // at paddle
+								FPPClient.WATERSPLASH.get(),
+								this.getX() + i * MathHelper.cos((float) Math.toRadians(this.getRotationClient().y - 10 + Math.random() * 20)),
+								(int) this.getY() + 1f,
+								this.getZ() + i * MathHelper.sin((float) Math.toRadians(this.getRotationClient().y - 10 + Math.random() * 20)),
+								(Math.random() - 0.5f) / 4f,
+								Math.random() * this.getVelocity().length() / 2f,
+								(Math.random() - 0.5f) / 4f
+						);
+					}
 					this.world.addParticle(        // at tail
-							ParticleTypes.CLOUD,
+							ParticleTypes.BUBBLE,
 							this.getX() - 1.2f * MathHelper.cos((float) Math.toRadians(this.getRotationClient().y + 90 + Math.random() * 30 * i)),
-							this.getY(),
+							(int) this.getY() + 0.5f + Math.random() / 2f,
 							this.getZ() - 1.2f * MathHelper.sin((float) Math.toRadians(this.getRotationClient().y + 90 + Math.random() * 30 * i)),
 							this.getVelocity().getX() / 5f,
-							Math.random() / 5f,
+							Math.random() / 3f,
 							this.getVelocity().getZ() / 5f
 					);
+				} else {
+					if (Math.random() > 0.5f) {
+						this.world.addParticle(		// at paddle
+								ParticleTypes.CLOUD,
+								this.getX() + i * MathHelper.cos((float) Math.toRadians(this.getRotationClient().y - 10 + Math.random() * 20)),
+								this.getY(),
+								this.getZ() + i * MathHelper.sin((float) Math.toRadians(this.getRotationClient().y - 10 + Math.random() * 20)),
+								Math.random() / 5f,
+								Math.random() / 5f,
+								Math.random() / 5f
+						);
+					} else {
+						this.world.addParticle(        // at tail
+								ParticleTypes.CLOUD,
+								this.getX() - 1.2f * MathHelper.cos((float) Math.toRadians(this.getRotationClient().y + 90 + Math.random() * 30 * i)),
+								this.getY(),
+								this.getZ() - 1.2f * MathHelper.sin((float) Math.toRadians(this.getRotationClient().y + 90 + Math.random() * 30 * i)),
+								this.getVelocity().getX() / 5f,
+								Math.random() / 5f,
+								this.getVelocity().getZ() / 5f
+						);
+					}
 				}
 			}
 		}
