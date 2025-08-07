9e72925179072c0a609411d10a56f7a8f4b10c55
Rimo <113169310+Rivmun@users.noreply.github.com>
2025-08-07 16:29:42 +0800

    O

--- common/src/main/java/com/rimo/footprintparticle/mixin/LivingEntityMixin.java
+++ common/src/main/java/com/rimo/footprintparticle/mixin/LivingEntityMixin.java
@@ -244,7 +244,7 @@ public abstract class LivingEntityMixin extends Entity {
 			}
 			if (!canGen) {
 				// Hardness Filter. See on https://minecraft.fandom.com/wiki/Breaking#Blocks_by_hardness
-				canGen = CONFIG.getLifeTimeAcc() > 0 && MathHelper.abs(block.getBlock().getHardness()) < CONFIG.getHardnessGate();
+				canGen = CONFIG.getHardnessGate() > 0 && MathHelper.abs(block.getBlock().getHardness()) < CONFIG.getHardnessGate();
 				if (canGen) {
 					canGen = ! CONFIG.getExcludedBlocks().contains(block.getBlock().getRegistryEntry().getKey().get().getValue().toString());
 					if (canGen) {
@@ -261,3 +261,4 @@ public abstract class LivingEntityMixin extends Entity {
 	}
 
 }
+
